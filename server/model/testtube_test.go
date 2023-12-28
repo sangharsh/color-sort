eb70e6e719e6b8e7d79d77b1c3992f68408eac1e
Sangharsh Boudhh <sboudhh@gmail.com>
2023-12-28 15:04:39 +0530

    Add Protobuf and gRPC
    * Use protobuf
    
    * Add tests
    
    * GRPC server creation
    
    * Delete unused file
    
    * Fix format
    
    * Add grpc args to protoc
    
    * Add new line at EOF
    
    * Clean up test log
    
    * Update go.sum
    
    ---------

--- server/model/testtube_test.go
+++ server/model/testtube_test.go
@@ -4,9 +4,31 @@ import (
 	"testing"
 )
 
+func TestIsCompleteNotFull(t *testing.T) {
+	tt := NewTesttube(4, []Color{Color_RED, Color_RED})
+	if tt.IsComplete() {
+		t.Fatalf(`Half tt returned as completed. tt: %v`, tt)
+	}
+}
+
+func TestIsCompleteDiffColors(t *testing.T) {
+	tt := NewTesttube(4, []Color{Color_RED, Color_RED, Color_RED, Color_GREEN})
+	if tt.IsComplete() {
+		t.Fatalf(`Diff colors tt returned as completed. tt: %v`, tt)
+	}
+}
+
+func TestPopEmpty(t *testing.T) {
+	tt := NewTesttube(4, []Color{})
+	_, err := tt.Pop()
+	if err == nil {
+		t.Fatalf(`Popped empty tt. tt: %v; Error: %v`, tt, err)
+	}
+}
+
 func TestAddColorValid(t *testing.T) {
-	tt := Testtube{4, []string{"red", "green"}}
-	color := "green"
+	tt := NewTesttube(4, []Color{Color_RED, Color_GREEN})
+	color := Color_GREEN
 	err := tt.AddColor(color)
 	if err != nil {
 		t.Fatalf(`Unable to add color %q to %v. Error: %v`, color, tt, err)
@@ -14,8 +36,8 @@ func TestAddColorValid(t *testing.T) {
 }
 
 func TestAddColorNonMatching(t *testing.T) {
-	tt := Testtube{4, []string{"red", "green"}}
-	color := "blue"
+	tt := NewTesttube(4, []Color{Color_RED, Color_GREEN})
+	color := Color_BLUE
 	err := tt.AddColor(color)
 	if err == nil {
 		t.Fatalf(`Added a non-matching color %q to %v.`, color, tt)
@@ -23,8 +45,8 @@ func TestAddColorNonMatching(t *testing.T) {
 }
 
 func TestAddColorFull(t *testing.T) {
-	tt := Testtube{4, []string{"red", "green", "blue", "yellow"}}
-	color := "blue"
+	tt := NewTesttube(4, []Color{Color_RED, Color_GREEN, Color_BLUE, Color_YELLOW})
+	color := Color_BLUE
 	err := tt.AddColor(color)
 	if err == nil {
 		t.Fatalf(`Added to a full testtube %v.`, tt)
