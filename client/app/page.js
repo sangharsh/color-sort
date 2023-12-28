b281432d293952df1e6ce16ea4468b4cf5d314aa
Sangharsh Boudhh <sboudhh@gmail.com>
2023-12-28 18:30:05 +0530

    Make gRPC call from client

--- client/app/page.js
+++ client/app/page.js
@@ -1,3 +1,18 @@
+'use client'
+
+import { LevelRequest } from '/gen/game_pb.js';
+import { ColorSortApiClient } from '/gen/game_grpc_web_pb.js';
+
 export default function Page() {
+    grpcCall();
     return <h1>Hello, Next.js!</h1>
+}
+
+function grpcCall() {
+    var service = new ColorSortApiClient('http://localhost:50051');
+    const request = new LevelRequest();
+    request.setLevel(1);
+
+    service.getGameLevel(request, {}, console.log);
+
 }
