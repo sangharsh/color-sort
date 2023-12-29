77b4ef105c75fd9ba46ab6a95ee5704ab3bbe118
Sangharsh Boudhh <sboudhh@gmail.com>
2023-12-29 14:36:44 +0530

    Working gRPC call from client via Envoy proxy

--- client/app/page.js
+++ client/app/page.js
@@ -9,10 +9,23 @@ export default function Page() {
 }
 
 function grpcCall() {
-    var service = new ColorSortApiClient('http://localhost:50051');
+    var service = new ColorSortApiClient('http://localhost:8080');
     const request = new LevelRequest();
     request.setLevel(1);
 
-    service.getGameLevel(request, {}, console.log);
+    const processResponse = (err, response) => {
+        console.log("err:", err, "response: ", response);
+        console.log("level:", response.getLevel());
+        console.log("tubes:", response.getTubesList());
+        const tubes = response.getTubesList();
+        const t0 = tubes[0];
+        console.log("t0:", t0);
+        const colors = t0.getColorsList()
+        console.log("colors:", colors);
+        const c0 = colors[0];
+        console.log("c0:", c0);
+    };
+
+    service.getGameLevel(request, {}, processResponse);
 
 }
