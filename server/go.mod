6ab12d5e33ee8347ea1429306d87cfaf6599337e
Sangharsh Boudhh <sboudhh@gmail.com>
2024-01-12 14:25:13 +0530

    Add server logs

--- server/go.mod
+++ server/go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"strconv"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/sangharsh/color-sort/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
@@ -26,7 +28,33 @@ func getPort() int {
 	return port
 }
 
+// InterceptorLogger adapts standard Go logger to interceptor logger.
+// This code is simple enough to be copied and not imported.
+func InterceptorLogger(l *log.Logger) logging.Logger {
+	return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
+		switch lvl {
+		case logging.LevelDebug:
+			msg = fmt.Sprintf("DEBUG :%v", msg)
+		case logging.LevelInfo:
+			msg = fmt.Sprintf("INFO :%v", msg)
+		case logging.LevelWarn:
+			msg = fmt.Sprintf("WARN :%v", msg)
+		case logging.LevelError:
+			msg = fmt.Sprintf("ERROR :%v", msg)
+		default:
+			panic(fmt.Sprintf("unknown level %v", lvl))
+		}
+		l.Println(append([]any{"msg", msg}, fields...))
+	})
+}
+
 func main() {
+	logger := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
+
+	logOpts := []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall),
+		// Add any other option (check functions starting with logging.With).
+	}
 	port := getPort()
 	log.Printf("Starting server at port %v", port)
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -34,6 +62,11 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
+	opts = append(opts, grpc.ChainUnaryInterceptor(
+		logging.UnaryServerInterceptor(InterceptorLogger(logger), logOpts...),
+		// Add any other interceptor you want.
+	))
+
 	grpcServer := grpc.NewServer(opts...)
 	api.Register(grpcServer)
 	// Register reflection service on gRPC server.
