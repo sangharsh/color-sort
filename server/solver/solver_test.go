622519aa4b326782517e2336abe97b87b842c8b8
Sangharsh Boudhh <sboudhh@gmail.com>
2024-01-23 13:56:51 +0530

    Use levels defined as textproto

--- server/solver/solver_test.go
+++ server/solver/solver_test.go
@@ -59,6 +59,30 @@ func TestDFSolver(t *testing.T) {
 func TestGreedySolver(t *testing.T) {
 
 	level := level.Generate(2)
+	solver := NewGreedySolver(level)
+	levelPlay := solver.Solve()
+
+	if !levelPlay.GetCurrentState().GetWon() {
+		t.Fatalf(`Game not won. Level: %v`, levelPlay.GetCurrentState().String())
+	} else {
+		t.Logf("Level: %v, NumMoves: %v", levelPlay.CurrentState.Id, len(levelPlay.Moves))
+		// t.Logf("LevelPlay: %v", levelPlay)
+		t.Logf("Moves: %v", MovesString(levelPlay))
+
+	}
+}
+
+func TestFileLevel(t *testing.T) {
+
+	level, err := level.GetLevelFromFile("../level/textpb/sample.textproto")
+	if err != nil {
+		t.Fatalf("Error while loading level: %v", err)
+	}
+
+	ok, err := model.HasValidColorAndTubes(level)
+	if !ok || err != nil {
+		t.Fatalf("Level invalid: %v; %v", ok, err)
+	}
 
 	solver := NewGreedySolver(level)
 	levelPlay := solver.Solve()
